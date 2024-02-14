b3b496c576bafde1769a76722be6f4282b5162df
optimist19 <oluwasegunbamidele3@gmail.com>
2024-02-14 14:27:02 +0100

    done with the layout, remaining fonts stling and responsiveness

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,6 +21,12 @@ import metaQuest from "../img/metaQuest.webp";
 import womanPic from "../img/vr-woman-pic.webp";
 import model from "../img/model.webp";
 import metaConnect from "../img/meta-connect.webp";
+import fb from "../img/fb.svg";
+import at from "../img/at.svg";
+import insta from "../img/insta.svg";
+import x from "../img/twitter.svg";
+import youtube from "../img/youtube.svg";
+import logo from "../img/Meta-Logo.png";
 
 import { PiCaretDownLight } from "react-icons/pi";
 
@@ -300,6 +306,168 @@ function Main() {
           </div>
         </div>
       </div>
+
+      <footer>
+        <div className="footer-con">
+          <div>
+            <div className="footer-logo-con">
+              <div className="footer-logo">
+                <div>
+                  <img src={logo} alt="" />
+                </div>
+              </div>
+              <div className="footer-socials-con">
+                <div>
+                  <img src={fb} alt="" />
+                </div>
+                <div>
+                  <img src={at} alt="" />
+                </div>
+                <div>
+                  <img src={insta} alt="" />
+                </div>
+                <div>
+                  <img src={x} alt="" />
+                </div>
+                <div>
+                  <img src={youtube} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="first-p">
+            <p>Site terms and policies</p>
+            <p>Community standards</p>
+            <p>Privacy policy</p>
+            <p>Terms</p>
+            <p>Cookie policy</p>
+            <p>Virtual reality</p>
+            <p>Shop Meta Quest</p>
+            <p>Refurbished Meta Quest 2</p>
+            <p>Meta Warranty Plus</p>
+            <p>VR for Good</p>
+            <p>Forums</p>
+            <p>Referrals</p>
+            <p>Blog</p>
+            <p>Creators</p>
+            <p>Download SDKs</p>
+            <p>Developers</p>
+            <p>Made for Meta partner program</p>
+            <p>Safety information for parents & pre-teens</p>
+            <p>Meta Quest health & safety information</p>
+            <p>Meta Quest safety center</p>
+            <p>Meta for Work</p>
+            <p>Meta Avatars</p>
+          </div>
+
+          <div className="second-p">
+            <p>Smart glasses</p>
+            <p>Shop Ray-Ban Meta smart glasses</p>
+            <p>Ray-Ban Stories</p>
+            <p>Privacy information</p>
+            <p>Supported countries</p>
+            <p>Support and legal</p>
+            <p>Meta Help Center</p>
+            <p>Order status</p>
+            <p>Returns</p>
+            <p>Find a product demo</p>
+            <p>Legal</p>
+            <p>Terms of sale</p>
+            <p>Our actions</p>
+            <p>Data and privacy</p>
+            <p>Safety and expression</p>
+            <p>Responsible business practices</p>
+            <p>Elections</p>
+            <p>COVID-19 response</p>
+            <p>Regulations</p>
+          </div>
+
+          <div className="third-p">
+            <p>About us</p>
+            <p>About Meta</p>
+            <p>Media gallery</p>
+            <p>Brand resources</p>
+            <p>For investors</p>
+            <p>Our community</p>
+            <p>Support SMB</p>
+            <p>Giving together</p>
+            <p>Social impact</p>
+            <p>App support</p>
+            <p>Facebook Help Center</p>
+            <p>Messenger Help Center</p>
+            <p>Instagram Help Center</p>
+            <p>WhatsApp Help Center</p>
+            <p>Workplace Help Center</p>
+            <p>Meta Verified</p>
+          </div>
+        </div>
+
+        <div className="second-footer">
+          <div className="united">
+            <p>United States &#40;English&#41;</p>
+          </div>
+          <div>
+            <p>META QUEST </p>
+            <p>
+              Meta Quest: *Parents:* Important guidance & safety
+              warnings for children&apos;s use here. Meta accounts for ages 10+
+              on Meta Quest 2 and 3; all other Meta Quest headsets for ages 13+.
+              Certain apps, games and experiences may be suitable for a more
+              mature audience. META QUEST FEATURES, FUNCTIONALITY, AND CONTENT
+              NOTICE: Features, functionality and content are subject to change
+              or withdrawal at any time, may not be available in all areas or
+              languages or may be restricted; may require enabled software or
+              service activation, and additional terms, conditions and/or
+              charges may apply.
+              <p>
+                Certain apps and experiences will be available by winter 2023.
+              </p>
+              <p>
+                META QUEST IMPORTANT SAFETY NOTICE{" "}
+                <a
+                  href="https://www.meta.com/quest/quest-2-facial-interface-recall/"
+                  target="_blank">
+                  {" "}
+                  https://www.meta.com/quest/quest-2-facial-interface-recall/
+                </a>
+                .
+              </p>
+              <p></p>Financing Options. You may be offered financing options for
+              your Meta purchases. Learn more <a href="#">here</a>.
+            </p>
+
+            <p>
+              *Based on the graphic performance of the Qualcomm Snapdragon XR2
+              Gen 2 vs XR2 Gen 1 on Meta Quest 2
+            </p>
+
+            <p>RAY-BAN | META SMART GLASSES</p>
+
+            <p>
+              Ray-Ban Meta smart glasses require a mobile phone with Android
+              (location services enabled) or iOS operating system, wireless
+              internet access, and account registration. Not compatible with all
+              smartphones. Ray-Ban Meta smart glasses only available in select
+              countries. Features, functionality and content are subject to
+              change or withdrawal at any time. Please visit{" "}
+              <a href="https://www.meta.com/smart-glasses/#faq" target="_blank">
+                https://www.meta.com/smart-glasses/#faq
+              </a>
+              for the most up-to-date information. For ages 13 and up only. This
+              product may interfere with personal medical devices. Requires
+              updates to be installed from time to time, including prior to
+              first use. While using Ray-Ban Meta smart glasses, it is always
+              the user&apos;s responsibility to comply with any local laws and
+              regulations, especially those relating to privacy and the use of
+              recording devices. Additional account registration, terms and fees
+              may apply.
+            </p>
+
+            <p>©2024 Meta.</p>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
